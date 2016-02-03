9398c1a70f773676c501d7e72bcf1e6e1bd00d17
Jamesjue <canjian.myself@gmail.com>
2016-02-03 17:06:05 +0000

    addd start and kill de mo scripts

---
+++ kill_demo.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo -e "killing gabriel..."
+gabriel=( "gabriel-upnp" "gabriel_REST_server" "faceswap-proxy.py" "gabriel-ucomm" "gabriel-control" )
+for i in "${gabriel[@]}"
+do
+    echo $i
+    PID=`ps -eaf | grep ${i} | grep -v grep | awk '{print $2}'`
+    if [[ "" !=  "$PID" ]]; then
+        echo "killing $PID"
+        kill -9 $PID
+    fi
+done
+echo -e "All killed"
---
+++ start_demo.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+debug=0
+while getopts "d" opt; do
+    case "$opt" in
+    d)  debug=1
+        ;;
+    esac
+done
+
+echo -e $gabriel_home
+gabriel_control="${gabriel_home}/bin"
+cd $gabriel_control &&
+./gabriel-control &
+sleep 3 &&
+./gabriel-ucomm &
+sleep 8 &&
+if pgrep -f "gabriel-ucomm" > /dev/null
+then
+    if [[ $debug -eq 1 ]];
+    then
+        echo 'debug mode...'
+        ./faceswap-proxy.py | tee faceswap.log
+    else
+        ./faceswap-proxy.py
+    fi
+else
+    ./kill_demo.sh
+    echo "launch failed"
+fi
+
